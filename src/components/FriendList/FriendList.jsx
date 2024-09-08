562644c88c4b06b5ec911880414871e03e955ded
TatyanaCheh <111444445+TatyanaCheh@users.noreply.github.com>
2024-09-08 16:50:37 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,22 @@
+import css from "./FriendList.module.css";
+import FriendListItem  from "../FriendListItem/FriendListItem";
+
+const FriendList = ({ friends }) => {
+    return (
+      <ul className={css.list}>
+        {/* Кількість li залежить від кількості об'єктів в масиві */}
+        {friends.map((friendItem) => {
+          return (
+            <li className={css.item} key={friendItem.id}>
+              <FriendListItem
+                avatar={friendItem.avatar}
+                name={friendItem.name}
+                isOnline={friendItem.isOnline}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
+  export default FriendList;
